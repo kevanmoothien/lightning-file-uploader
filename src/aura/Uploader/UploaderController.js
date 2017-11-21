9db23c2ce4c8093ce0c9651e84234b003d91f2cd
Kevan <kevan.moothien@spoonconsulting.com>
2017-11-21 23:52:30 +0400

    Update UploaderController.js

--- src/aura/Uploader/UploaderController.js
+++ src/aura/Uploader/UploaderController.js
@@ -2,6 +2,6 @@
 	handleUploadFinished: function (cmp, event) {
         // Get the list of uploaded files
         var uploadedFiles = event.getParam("files");
-        alert("Files uploaded : " + uploadedFiles.length);
+        console.log("Files uploaded : " + uploadedFiles.length);
     }
 })
